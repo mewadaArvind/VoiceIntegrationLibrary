e0c3159d804db418e321cbf1b5b304b0e5aedc99
Arvind Rajput <singhsubak9926@gmail.com>
2019-06-05 10:52:55 +0530

    voice input helper and permission libraray remove library

--- voicelibrarysample/src/main/java/com/example/voicelibrarysample/VoiceHelper/VoiceHelper.java
+++ voicelibrarysample/src/main/java/com/example/voicelibrarysample/VoiceHelper/VoiceHelper.java
@@ -8,8 +8,9 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
-import android.support.annotation.Nullable;
+
 import android.util.Log;
+
 
 import com.example.voicelibrarysample.VoiceMasterClass;
 
@@ -18,57 +19,56 @@ import java.util.List;
 
 import static android.speech.SpeechRecognizer.createSpeechRecognizer;
 
-public class VoiceHelper  implements StopListening, Processing, Listening{
+public class VoiceHelper  {
 
     private SpeechRecognizer speechRecognizer = null;
     private TextToSpeech tts = null;
-    private UtteranceProgressListener utteranceProgressListener;
     private boolean isSpeechRecognizerRunning;
     private Context context;
     private VoiceMasterClass voiceMasterClass;
 
-    public VoiceHelper(Context context, VoiceMasterClass voiceMasterClass){
+    public VoiceHelper(Context context, VoiceMasterClass voiceMasterClass) {
         this.voiceMasterClass = voiceMasterClass;
         this.context = context;
         speechRecognizer = createSpeechRecognizer(context);
     }
 
 
-    public boolean isCurrentlySpeakingMain(){
-        if(tts != null && tts.isSpeaking()){
+    public boolean isCurrentlySpeakingMain() {
+        if (tts != null && tts.isSpeaking()) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public boolean isCurrentlyListeningMain(){
+    public boolean isCurrentlyListeningMain() {
         return isSpeechRecognizerRunning;
     }
 
-    public void startSpeakingMain(String msg, String utteranceID){
-        tts.speak(msg,TextToSpeech.QUEUE_FLUSH,null,utteranceID);
+    public void startSpeakingMain(String msg, String utteranceID) {
+        tts.speak(msg, TextToSpeech.QUEUE_FLUSH, null, utteranceID);
     }
 
-    public void stopSpeakingMain(){
+    public void stopSpeakingMain() {
         isSpeechRecognizerRunning = false;
-        if(tts != null && tts.isSpeaking()){
+        if (tts != null && tts.isSpeaking()) {
             tts.stop();
         }
     }
 
-    public void stopListeningMain(){
-        if(speechRecognizer != null){
+    public void stopListeningMain() {
+        if (speechRecognizer != null) {
             speechRecognizer.stopListening();
             speechRecognizer.cancel();
             speechRecognizer.destroy();
         }
-        /*stop close listening*/
-        close();
+        voiceMasterClass.stopListening();
+//        close();
         voiceMasterClass.getMasterInterfaceVoice().stopListening();
     }
 
-    public void startListeningMain(){
+    public void startListeningMain() {
         if (speechRecognizer != null) {
             speechRecognizer.stopListening();
             speechRecognizer.cancel();
@@ -76,13 +76,13 @@ public class VoiceHelper  implements StopListening, Processing, Listening{
             speechRecognizer.setRecognitionListener(new RecognitionListener() {
                 @Override
                 public void onReadyForSpeech(Bundle bundle) {
-                    isSpeechRecognizerRunning  = true;
-                    data(null, null);
+                    isSpeechRecognizerRunning = true;
+//                    data(null, null);
                 }
 
                 @Override
                 public void onBeginningOfSpeech() {
-                    data(null, null);
+//                    data(null, null);
                 }
 
                 @Override
@@ -96,13 +96,15 @@ public class VoiceHelper  implements StopListening, Processing, Listening{
 
                 @Override
                 public void onEndOfSpeech() {
-                    close();
+//                    close();
+                    voiceMasterClass.stopListening();
                     isSpeechRecognizerRunning = false;
                 }
 
                 @Override
                 public void onError(int i) {
-                    close();
+//                    close();
+                    voiceMasterClass.stopListening();
                     isSpeechRecognizerRunning = false;
                 }
 
@@ -121,7 +123,9 @@ public class VoiceHelper  implements StopListening, Processing, Listening{
                         Log.d("VOICE_TAG", a + ": " + confArr[i]);
                         i++;
                     }
-                    data(null, msg);
+                    Log.e("Final...",msg);
+
+//                    data(null, msg);
 
                 }
 
@@ -129,7 +133,7 @@ public class VoiceHelper  implements StopListening, Processing, Listening{
                 public void onPartialResults(Bundle bundle) {
                     List<String> arrayList = (List<String>) bundle.get("results_recognition");
                     String msg = arrayList.get(0);
-                    data(msg,null);
+//                    data(msg, null);
                 }
 
                 @Override
@@ -141,15 +145,14 @@ public class VoiceHelper  implements StopListening, Processing, Listening{
             final Intent recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             recognizerIntent.putExtra(
                     RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-IN");
-            recognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS,true);
+            recognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
             recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, (context).getPackageName());
             speechRecognizer.startListening(recognizerIntent);
             voiceMasterClass.getMasterInterfaceVoice().startedListening();
         }
-        startListeningMain();
     }
 
-    public void destroyMain(){
+    public void destroyMain() {
         if (tts != null) {
             tts.stop();
             tts.shutdown();
@@ -157,18 +160,4 @@ public class VoiceHelper  implements StopListening, Processing, Listening{
         this.voiceMasterClass.getMasterInterfaceVoice().destroy();
     }
 
-    @Override
-    public void data(@Nullable String partialResult, @Nullable String fullResult) {
-        data(partialResult,fullResult);
-    }
-
-    @Override
-    public void run(String finalMessage) {
-            run(finalMessage);
-    }
-
-    @Override
-    public void close() {
-        close();
-    }
 }
